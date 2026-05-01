88ceaa4611d43a1113e02c2b5ef505592b002397
ankit gill <gillankit076@gmail.com>
2026-05-01 17:20:27 +0530

    fix(auth): update lastActive only after session validation

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -7,13 +7,16 @@ export async function checkAuth(req, res, next) {
   const { sid } = req.signedCookies;
   try {
     if (!sid) {
-      return res.status(401).json({ error: "User not found" });
+      return res.status(401).json({ error: "Unauthorized" });
     }
+
     const session = await redisClient.json.get(`session:${sid}`);
-    await redisClient.json.set(`session:${sid}`, "$.lastActive", Date.now());
+
     if (!session) {
-      return res.status(401).json({ error: "User not found" });
+      return res.status(401).json({ error: "Session expired. Please log in again." });
     }
+    await redisClient.json.set(`session:${sid}`, "$.lastActive", Date.now());
+
     const user = await Users.findById(session.userId);
 
     if (!user) {
@@ -25,6 +28,7 @@ export async function checkAuth(req, res, next) {
         "Your account is deactivated. Please contact support for reactivation.",
       );
     }
+
     req.user = user;
     next();
   } catch (error) {
