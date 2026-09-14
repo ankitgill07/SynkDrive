13bfc813c86507889f4562f14390b646bfaaa835
Ankit Gill <166650090+ankitgill07@users.noreply.github.com>
2026-09-14 12:19:44 +0530

    Update allowed origins for CORS
    Added production URLs to the allowed origins list.

--- server/app.js
+++ server/app.js
@@ -37,8 +37,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
-  "http://127.0.0.1:5173",
-  "http://127.0.0.1:5174",
+  "https://synkdrive.in",
+  "https://www.synkdrive.in",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
